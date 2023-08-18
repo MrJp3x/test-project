550b2268326af4002cef47ffeb563d9422fbbe4f
Pedix <amirbbasasgary9731@gmail.com>
2023-08-18 19:28:13 +0330

    dobare branch sakhtam va bayad add beshan.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import methods
 
 methods.say_hello('pedram')
-
+methods.say_hello('mostafa')
--- methods.py
+++ methods.py
@@ -1,3 +1,6 @@
 def say_hello(name: str):
     print(f'hello {name}')
     return None
+
+def say_goodbye(name):
+    return f'goodbye {name}'
